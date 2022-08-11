7b72e6773f1cde27974673c5b519914b61980f4f
HWorrell <31424637+HWorrell@users.noreply.github.com>
2022-08-11 18:13:49 -0400

    Update OtherQuiz.java

--- OtherQuiz.java
+++ OtherQuiz.java
@@ -7,71 +7,51 @@ public class OtherQuiz {
     int rounds;
     RomajiParser romajiParser;
 
-    OtherQuiz(Vector<Term> tVector, int r, RomajiParser parser){
-        this.termVector = tVector;
+    OtherQuiz(Vector<Term> tvector, int r, RomajiParser parser){
+        this.termVector = tvector;
         this.rounds = r;
         this.romajiParser = parser;
     }
 
-    /*
-    this version is designed for a person that cannot read hiragana or katakana
-    all resources are populated in Main, and this is primarily concerned with
-    running the quiz itself, and the logic for that.  The resources populated
-    are as follows:
-
-    Vector of "Term" objects
-    integer number of rounds
-    a populated Romaji Parser
-
-     */
     void runOtherQuiz(){
-        //this holds the questions that have been tested on previously
         Vector<Integer> testedQuestions = new Vector<>();
-        //RNG
         Random random = new Random();
-        //main loop that drives the quiz
+        int checkPeriod;
+        if (rounds/5 <20){
+            checkPeriod = rounds/5;
+        }
+        else{
+            checkPeriod = 20;
+        }
+        int checkCounter = 0;
         for (int i = 0; i < this.rounds; i++){
-            //get a random int between 0 and the number of questions
+
+            checkCounter++;
+
+            if (this.rounds > 20) {
+                if (checkCounter == checkPeriod) {
+                    String cont = JOptionPane.showInputDialog("Continue? Enter \"no\" to quit, or yes to continue");
+                    if (cont.equals("no")) {
+                        System.exit(0);
+                    } else {
+                        checkCounter = 0;
+                    }
+                }
+            }
             int question = random.nextInt(this.termVector.size());
-            //if we've already tested on this question
             if (testedQuestions.contains(question)){
-                //70% chance to repick the question.  there is value in some repeats, but
-                //I'd prefer them to be relatively rare
                 if (random.nextInt(10) > 3){
-                    //if repick, deincrement the loop and re-run
                     i--;
                     continue;
                 }
             } else{
-                //if we haven't seen this question before, add it to the tested questions we've seen
                 testedQuestions.add(question);
             }
-            //if number of tested questions == number of terms, reset the questions we've seen
             if (testedQuestions.size() == this.termVector.size()){
                 testedQuestions.clear();
             }
-            //determine if the term in question has a kanji associated in the parameters provided
             boolean hasKanji = !termVector.elementAt(question).getKanji().equals("");
 
-            /*
-            if it has a kani, there are 6 cases we can test on, as there are three parameters:
-            English Definition
-            Reading
-            Kanji
-
-            Definition - Kanji
-            Definition - Reading
-            Reading - Kanji
-            Reading - Definition
-            Kanji - Definition
-            Kanji - Reading
-
-            This is run via a switch statement.  There's probably a more clean way to write this, but
-            this is good enough for now.
-
-            TODO: Make this better
-
-             */
             if (hasKanji){
                 switch (random.nextInt(6)){
                     //see kanji, produce reading
@@ -92,7 +72,7 @@ public class OtherQuiz {
                     //see english definition, produce reading
                     case 3:
                         JOptionPane.showMessageDialog(null, "What is the reading for this definition: " + this.termVector.elementAt(question).getEnglishDefinition() + "?");
-                        JOptionPane.showMessageDialog(null, this.romajiParser.parseTerm(this.termVector.elementAt(question).getKanaReading()));
+                        JOptionPane.showMessageDialog(null, this.romajiParser.parseTerm(this.romajiParser.parseTerm(this.termVector.elementAt(question).getKanaReading())));
                         break;
                     //see reading, produce kanji
                     case 4:
@@ -106,11 +86,6 @@ public class OtherQuiz {
                         break;
                 }
             } else{
-                /*
-                If there's no kanji, there are only two possible cases:
-                English Definition - Reading
-                Reading - English Definition
-                 */
                 switch (random.nextInt(2)){
 
                     //see english definition, produce reading
